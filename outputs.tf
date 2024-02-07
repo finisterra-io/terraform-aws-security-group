4f91882c99bab4cff15e1703407682bc34e2f688
Daniel J <daniel@finisterra.io>
2024-02-06 22:13:26 -0500

    Update outputs.tf to use default security group values

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "The created or target Security Group ID"
-  value       = local.security_group_id
+  value       = aws_security_group.default[0].id
 }
 
 output "arn" {
   description = "The created Security Group ARN (null if using existing security group)"
-  value       = try(local.created_security_group.arn, null)
+  value       = aws_security_group.default[0].arn
 }
 
 output "name" {
   description = "The created Security Group Name (null if using existing security group)"
-  value       = try(local.created_security_group.name, null)
+  value       = aws_security_group.default[0].name
 }
