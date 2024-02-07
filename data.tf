561cc6813b81ddc6ddde5dcae655aeca138d93ca
Julian <eng@finisterra.io>
2024-02-06 22:08:09 -0500

    Update Terraform and AWS versions

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 
 data "aws_vpc" "default" {
-  count = module.this.enabled && var.vpc_name != null ? 1 : 0
+  count = var.enabled && var.vpc_name != null ? 1 : 0
   tags = {
     Name = var.vpc_name
   }
